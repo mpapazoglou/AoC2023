523b6d2ab6d08d74e488249fa692ac3fe2239e7c
Michael Papazoglou <mpapazoglou@wrsystems.com>
2023-12-12 07:11:49 -0500

    re-did day 5

--- day5/puzzle2mp.py
+++ day5/puzzle2mp.py
@@ -13,7 +13,7 @@ with open('input.txt','rt') as f:
         line = line.rstrip()
         if line.startswith('seeds:'):
             s = [int(s) for s in line.split(':')[-1].split()]
-            seed_ranges = [range(s[i],s[i]+s[i+1]) for i in range(0,len(s),2)]
+            seed_list = [range(s[i],s[i]+s[i+1]) for i in range(0,len(s),2)]
 
         elif line.endswith('map:'):
             slist = line.split(':')[0].split(' ')[0].split('-')
@@ -24,35 +24,32 @@ with open('input.txt','rt') as f:
             maps[-1]['src'].append(range(x[1],x[1]+x[2]))
             maps[-1]['offset'].append(x[0]-x[1])
 
-def location2seed_fun(loc,maps):
-    src = loc
-    for m in maps[::-1]:
-        dst = src
-        for i, s_ in enumerate(m['src']):
-            if src - m['offset'][i] in s_:
-                src = dst - m['offset'][i]
-                break
-    return src
+def seed_location_fun(seed,maps,index,ret):
+    print('seed_location_fun, start seed is %d'%(seed[0]))
+    loc = math.inf
+    for s in seed:
+        src = s
+        for m in maps:
+            dst = src
+            for i, s_ in enumerate(m['src']):
+                if src in s_:
+                    dst = src + m['offset'][i]
+                    break
+            src = dst
+        loc = min(dst,loc)
+    print('loc: %d'%(loc))
+    ret[index] = loc
 
-def check_seed(seed,seed_ranges):
-    ret = False
-    for s in seed_ranges:
-        if seed in s:
-            ret = True
-            break
-    return ret
-        
-# search over locations to find initial seed, and check if it's in any of the the
-# seed ranges
-ret = False
-loc = -1
-while not ret:
-    loc += 1
-    if math.remainder(loc,500000)==0:
-        print('loc: %d'%(loc))
-    s = location2seed_fun(loc,maps)
-    ret = check_seed(s,seed_ranges)
-    
+manager = mp.Manager()
+ret = manager.dict()
+jobs = [mp.Process(target=seed_location_fun,args=(s,maps,i,ret)) for i,s in enumerate(seed_list)]   
+for j in jobs:
+    j.start()
+
+for j in jobs:
+    j.join()
+
+loc = min(ret.values())
 etime = time.time() - start_time
 print('answer is %d, elapsed time: %.3f minutes'%(loc,etime/60))
 
